5a1e1432376bbd5d06fed030ce7851dc398243db
Erick Rivera <yomellamoerick.771@gmail.com>
2023-11-13 22:20:19 -0300

    controller edited and secret created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'sign_up'
+  },
+  controllers:{
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 end
