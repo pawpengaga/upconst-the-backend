afbbf201c8d5d7d61d18ab3a22ed2d6c4c694b60
Erick Rivera <yomellamoerick.771@gmail.com>
2023-11-13 22:31:42 -0300

    first version of the users api

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,20 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
+  include RackSessionsFix
   respond_to :json
+
+  private
+
+  def respond_with(current_user, _opts = {})
+    if resource.persisted?
+      render json: {
+        status: {code: 200, message: 'Registro satisfactorio.'},
+        data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
+      }
+      else
+        render json: {
+          status: {message: "Usuario no pudo ser creado. #{current_user.errors.full_messages.to_sentence}"},
+          status: :unprocessable_entity
+        }
+    end
+  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,34 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
+  include RackSessionsFix
   respond_to :json
+
+  private
+
+  def respond_with(current_user, _opts = {})
+    render json: {
+      status: {
+        code: 200, message: 'Inicio de sesión exitoso.',
+        data: { user: UserSerializer.new(current_user).serializable_hash[:data][:attributes] }
+      }
+    }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    if request.headers['Authorization'].present?
+      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, ENV["devise_jwt_secret_key"]).first
+      current_user = User.find(jwt_payload['sub'])
+    end
+
+    if current_user
+      render json: {
+        status: 200,
+        message: 'Cierre de sesión exitoso'
+      }, status: :ok
+      else
+        render json: {
+          status: 401,
+          message: "No se ha encontrado la sesión"
+        }, status: :unauthorized
+    end
+  end
 end
