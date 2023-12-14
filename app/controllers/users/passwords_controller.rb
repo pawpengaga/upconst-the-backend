bef47db73026e929f45f31fd6f0d1e416b7a69c2
Erick Rivera <yomellamoerick.771@gmail.com>
2023-12-14 16:31:39 -0300

    update made better for login

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 
 class Users::PasswordsController < Devise::PasswordsController
+  before_action :configure_permitted_parameters
   respond_to :json
+
+  def update
+    user = User.find_by(reset_password_token: params[:reset_password_token])
+
+    if user && user.reset_password_period_valid?
+      if user.update(password: params[:password], password_confirmation: params[:password_confirmation])
+        render json: { message: 'Contraseña actualizada exitosamente' }
+      else
+        render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: 'Token inválido o expirado' }, status: :unprocessable_entity
+    end
+  end
 
   def forgot
     if params[:email].blank? # check if email is present
@@ -18,34 +33,10 @@ class Users::PasswordsController < Devise::PasswordsController
       render json: {error: 'Email, no valido, lo has perdido todo XDDDDD'}, status: :not_found
     end
   end
-  # GET /resource/password/new
-  # def new
-  #   super
-  # end
 
-  # POST /resource/password
-  # def create
-  #   super
-  # end
+  private
 
-  # GET /resource/password/edit?reset_password_token=abcdef
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource/password
-  # def update
-  #   super
-  # end
-
-  # protected
-
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sending reset password instructions
-  # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[password password_confirmation reset_password_token])
+  end
 end
