6b46818834319e492f7535ccdb532aea2492cdad
Erick Rivera <yomellamoerick.771@gmail.com>
2023-12-14 16:01:28 -0300

    suffix-fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   #config.action_mailer.default_options = {from: 'no-reply@testemail.com'}
-  config.action_mailer.default_url_options = { host: ENV["frontend_url"] }
+  config.action_mailer.default_url_options = { host: ENV["frontend_url"].delete_suffix('/') }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
