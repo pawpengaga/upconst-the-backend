3ca743c4a30aa7d5b08d4d30be14a5b58ce93edc
Erick Rivera <yomellamoerick.771@gmail.com>
2023-12-14 14:12:46 -0300

    maileroptions

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,9 @@
 class UserMailer < ApplicationMailer
+  default_url_options[:host] = Rails.application.config.action_mailer.default_url_options[:host]
+
   def password_reset(user)
     @user = user
+    @reset_url = edit_user_password_url(@user, reset_password_token: @user.reset_password_token)
     mail(to: @user.email, subject: 'Recuperar contraseña - UpConst')
   end
 end
