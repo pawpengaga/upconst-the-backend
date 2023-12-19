ebfa9d0d120c97196f6ea4ead98e1646ec7e8446
Erick Rivera <yomellamoerick.771@gmail.com>
2023-12-19 20:15:39 -0300

    super modified. part 5 COMMENTS

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -5,19 +5,27 @@ class Users::PasswordsController < Devise::PasswordsController
   respond_to :json
 
   def update
+    puts "=========== Entering update method ==========="
+
     user = User.find_by(reset_password_token: params[:reset_password_token])
 
     if user && user.reset_password_period_valid? && (user.reset_password_token == params[:reset_password_token])
+      puts "=========== Token and period are valid ==========="
+
       if user.update(password: params[:password], password_confirmation: params[:password_confirmation])
+        puts "=========== Password updated successfully ==========="
         user.clear_reset_password_token
         render json: { success: true, message: 'Contraseña actualizada exitosamente', errors: [] }
       else
+        puts "=========== Error updating password ==========="
         render json: { success: false, message: 'Error al actualizar la contraseña', errors: user.errors.full_messages }, status: :unprocessable_entity
       end
     else
+      puts "=========== Token invalid or expired ==========="
       render json: { success: false, message: 'Token inválido o expirado', errors: ['Token inválido o expirado'] }, status: :unprocessable_entity
     end
   end
+
 
   def forgot
     if params[:email].blank? # check if email is present
